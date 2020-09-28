bc21ee939f3f34dabe47c2e0d05335f3e9dfc0e1
SungmiKim <62002100+SungmiKim@users.noreply.github.com>
2020-09-28 17:50:32 +0900

    2020-09-28
    2020-09-28

--- screens/Tv/TvPresenter.js
+++ screens/Tv/TvPresenter.js
@@ -2,30 +2,37 @@ import React from "react";
 import HorizontalSlider from "../../components/HorizontalSlider";
 import ScrollContainer from "../../components/ScrollContainer";
 import Vertical from "../../components/Vertical";
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  margin-top: 30px;
+`;
 
 export default ({ loading, popular, topRated }) => (
   <ScrollContainer loading={loading}>
-    <HorizontalSlider title={"Popular Shows"}>
-      {popular.map((show) => (
-        <Vertical
-          id={show.id}
-          key={show.id}
-          poster={show.poster_path}
-          title={show.name}
-          votes={show.vote_average}
-        />
-      ))}
-    </HorizontalSlider>
-    <HorizontalSlider title={"Top Rated"}>
-      {topRated.map((show) => (
-        <Vertical
-          id={show.id}
-          key={show.id}
-          poster={show.poster_path}
-          title={show.name}
-          votes={show.vote_average}
-        />
-      ))}
-    </HorizontalSlider>
+    <Container>
+      <HorizontalSlider title={"Popular Shows"}>
+        {popular.map((show) => (
+          <Vertical
+            id={show.id}
+            key={show.id}
+            poster={show.poster_path}
+            title={show.name}
+            votes={show.vote_average}
+          />
+        ))}
+      </HorizontalSlider>
+      <HorizontalSlider title={"Top Rated"}>
+        {topRated.map((show) => (
+          <Vertical
+            id={show.id}
+            key={show.id}
+            poster={show.poster_path}
+            title={show.name}
+            votes={show.vote_average}
+          />
+        ))}
+      </HorizontalSlider>
+    </Container>
   </ScrollContainer>
 );
